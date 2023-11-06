f1513cfdbb7dfdaf6aa0909e521225559dfa8f96
lucasjensch <92726945+lucasjensch@users.noreply.github.com>
2023-11-06 18:09:12 +0100

    Committed changes to the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React, {useState} from 'react';
 import './App.css'
+
+
 
 function App() {
 
+  const [popup, openPopup] = React.useState(false);
 
   return (
     <>
@@ -25,13 +29,24 @@ function App() {
               <img className="h-48 w-full object-cover md:h-full md:w-48" src="./src/assets/Hintergrund_Haus.jpg" alt="Modern building architecture" />
             </div>
             <div className="p-8">
-              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Rund ums Haus</div>
               <p className="block mt-1 text-lg leading-tight font-medium text-black">Incredible accommodation for your team</p>
               <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
-              <button className='pt-4 text-indigo-500 font-bold'>Mehr Informationen </button>
+              <button onClick ={() => openPopup(true)} className='pt-4 text-indigo-500 font-bold'>Mehr Informationen </button>
             </div>
           </div>
         </div>
+        
+        {popup ?<div className="fixed top-0 bg-black/50 w-full h-screen flex items-center justify-center z-99">
+            <div className="bg-white w-10/12 h-2/3 flex justify-center">
+              
+              <button onClick ={() => openPopup(false)} className='absolute w-12 h-12' ><img className='' src="src/assets/icon-close.svg" alt="close" /></button>
+              <div className='p-5'>
+                <h2 className='text-3xl font-semibold'>Rund ums Haus</h2>
+                <p>Auf der Terrasse befinden sich Gartenmobilar und ein Strandkorb</p>
+              </div>
+            </div>
+          </div> :null}
 
         <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-6">
           <div className="md:flex">

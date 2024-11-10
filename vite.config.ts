d134c97ff51348ec688a9d36f96c8443d31ac63e
Lucas.jsch <lucasjensch@gmail.com>
2024-11-10 20:38:34 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        anfrage: resolve(__dirname, 'pages/anfrage.html'),
+        datenschutz: resolve(__dirname, 'pages/datenschutz.html'),
+        preistabelle: resolve(__dirname, 'pages/preistabelle.html')
+      }
+    }
+
+  }
 })
